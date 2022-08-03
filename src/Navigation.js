910b775ddbf0c7763059142190666da2c592014e
Агахан05 <avazovsk04@gmail.com>
2022-08-03 18:57:22 +0600

    Весь Админ часть сделал

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminProvider from "./contexts/AdminProvider";
 import Navbar from "./Navbar";
+import AdminAddPages from "./Pages/AdminAddPages";
+import AdminEditPages from "./Pages/AdminEditPages";
+import AdminPages from "./Pages/AdminPages";
 import MainPages from "./Pages/MainPages";
 
 function Navigation() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MainPages />} />
-      </Routes>
-    </BrowserRouter>
+    <AdminProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPages />} />
+          <Route path="/admin" element={<AdminPages />} />
+          <Route path="/admin/add" element={<AdminAddPages />} />
+          <Route path="/admin/edit/:id" element={<AdminEditPages />} />
+        </Routes>
+      </BrowserRouter>
+    </AdminProvider>
   );
 }
 
